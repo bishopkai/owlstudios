743909b797d3a5f85ff9a6eea6f160646b01ad0f
bishopkai <123687632+bishopkai@users.noreply.github.com>
2024-01-08 16:34:53 -0500

    Update Navbar.jsx

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
-          className="text-2xl md:text-4xl text-white font-semibold"
+          className="text-xl md:text-3xl text-white font-semibold"
         >
                   <Image src="/images/logo.png" width={50} height={50} alt="Mr.Owl Studios" />
           MR. OWL STUDIOS
