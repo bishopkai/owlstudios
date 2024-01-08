7057906293396a867567f4ead534520b92e44868
bishopkai <123687632+bishopkai@users.noreply.github.com>
2024-01-08 15:38:36 -0500

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,15 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>MR. OWL STUDIOS</span>
+                <span>
+        <Image
+              src="/images/logo.png"
+              alt="logo"
+              width={50}
+              height={50}
+            />
+                  MR. OWL STUDIOS
+        </span>
         <p className="text-slate-600">| &copy; 2024 Mr. Owl Studios - All rights reserved. |</p>
       </div>
     </footer>
