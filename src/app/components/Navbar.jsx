787c8b11bd6357db4b80baf561ce6263596269d7
bishopkai <123687632+bishopkai@users.noreply.github.com>
2024-01-08 16:13:32 -0500

    Update Navbar.jsx

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,7 +4,8 @@ import React, { useState } from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
-import Image from "next/image/dist/image";
+import { Image } from "next";
+
 
 
 const navLinks = [
